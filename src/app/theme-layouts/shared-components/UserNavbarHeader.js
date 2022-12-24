a7c53c3db6f959e6e826c797faef79cc897226af
Muhammad Shahnoor <SP20-BCS-137@CUILAHORE.EDU.PK>
2022-12-24 11:15:26 +0500

    Remove Navbar Image

--- src/app/theme-layouts/shared-components/UserNavbarHeader.js
+++ src/app/theme-layouts/shared-components/UserNavbarHeader.js
@@ -1,26 +1,26 @@
-import { styled } from '@mui/material/styles';
-import Avatar from '@mui/material/Avatar';
-import Typography from '@mui/material/Typography';
-import { useSelector } from 'react-redux';
-import { selectUser } from 'app/store/userSlice';
+import { styled } from "@mui/material/styles";
+import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
+import { useSelector } from "react-redux";
+import { selectUser } from "app/store/userSlice";
 
-const Root = styled('div')(({ theme }) => ({
-  '& .username, & .email': {
-    transition: theme.transitions.create('opacity', {
+const Root = styled("div")(({ theme }) => ({
+  "& .username, & .email": {
+    transition: theme.transitions.create("opacity", {
       duration: theme.transitions.duration.shortest,
       easing: theme.transitions.easing.easeInOut,
     }),
   },
 
-  '& .avatar': {
+  "& .avatar": {
     background: theme.palette.background.default,
-    transition: theme.transitions.create('all', {
+    transition: theme.transitions.create("all", {
       duration: theme.transitions.duration.shortest,
       easing: theme.transitions.easing.easeInOut,
     }),
     bottom: 0,
-    '& > img': {
-      borderRadius: '50%',
+    "& > img": {
+      borderRadius: "50%",
     },
   },
 }));
@@ -31,22 +31,15 @@ function UserNavbarHeader(props) {
   return (
     <Root className="user relative flex flex-col items-center justify-center p-16 pb-14 shadow-0">
       <div className="flex items-center justify-center mb-24">
-        <Avatar
-          sx={{
-            backgroundColor: 'background.paper',
-            color: 'text.secondary',
-          }}
-          className="avatar text-32 font-bold w-96 h-96"
-          src={user.data.photoURL}
-          alt={user.data.displayName}
-        >
-          {user.data.displayName.charAt(0)}
-        </Avatar>
+        {/* Image removed from here */}
       </div>
       <Typography className="username text-14 whitespace-nowrap font-medium">
         {user.data.displayName}
       </Typography>
-      <Typography className="email text-13 whitespace-nowrap font-medium" color="text.secondary">
+      <Typography
+        className="email text-13 whitespace-nowrap font-medium"
+        color="text.secondary"
+      >
         {user.data.email}
       </Typography>
     </Root>
