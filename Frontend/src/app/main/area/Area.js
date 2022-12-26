0460a27a1a9c08457fd03e05aecafb1bc5a70d34
Muhammad Shahnoor <SP20-BCS-137@CUILAHORE.EDU.PK>
2022-12-26 23:29:47 +0500

    Add styles for content in area page

--- Frontend/src/app/main/area/Area.js
+++ Frontend/src/app/main/area/Area.js
@@ -1,6 +1,6 @@
 import { styled } from "@mui/material/styles";
 import FusePageSimple from "@fuse/core/FusePageSimple";
-import AreasTable from "./Table";
+import AreasData from "./Table";
 import AddButton from "./Button";
 
 const Root = styled(FusePageSimple)(({ theme }) => ({
@@ -20,9 +20,40 @@ function Dashbaord(props) {
   return (
     <Root
       content={
-        <div className="p-24">
-          <AddButton />
-          <AreasTable />
+        <div
+          className="p-24"
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "flex-start",
+            width: "100%",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              width: "100%",
+              justifyContent: "flex-end",
+              paddingBottom: 10,
+              paddingRight: 30,
+            }}
+          >
+            <AddButton />
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              width: "100%",
+              justifyContent: "center",
+              paddingTop: 10,
+              paddingBottom: 10,
+              paddingRight: 30,
+            }}
+          >
+            <AreasData />
+          </div>
         </div>
       }
       scroll="content"
