f7688a04f5160be5c23705676e98a593a4d49e17
Hammad Nazir <hammadnazir106@gmail.com>
2022-12-27 22:32:02 -0800

    Make Post and Get Method For Package

--- Backend/server/server.js
+++ Backend/server/server.js
@@ -1,80 +1,111 @@
-const express = require('express')
-const app = express()
-var cors = require('cors');
+const express = require("express");
+const app = express();
+var cors = require("cors");
 
-const port = 3001
-const mongoose = require('mongoose')
-const Area = require('../Modles/areaModel')
- 
-app.use(express.json())
+const port = 3001;
+const mongoose = require("mongoose");
+const Area = require("../Modles/areaModel");
+const Area = require("../Modles/packageModel");
+
+app.use(express.json());
 app.use(cors());
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get("/", (req, res) => res.send("Hello World!"));
 
-app.get('/', (req, res) => res.send('A Test String'))
+app.get("/", (req, res) => res.send("A Test String"));
 
-app.post('/testp', (req, res) => res.send('A Test String form post'))
+app.post("/testp", (req, res) => res.send("A Test String form post"));
 
-app.post('/addArea', async (req, res) => {
-    const {code, name} = req.body
-    try{
-        const area = await Area.create({code, name})
-        res.send(area)
-    }catch(err){
-        res.send("Error")
-        console.log(err.message)
+app.post("/addArea", async (req, res) => {
+  const { code, name } = req.body;
+  try {
+    const area = await Area.create({ code, name });
+    res.send(area);
+  } catch (err) {
+    res.send("Error");
+    console.log(err.message);
+  }
+});
+
+app.post("/addArea", async (req, res) => {
+  const { code, name } = req.body;
+  try {
+    const area = await Area.create({ code, name });
+    res.send(area);
+  } catch (err) {
+    res.send("Error");
+    console.log(err.message);
+  }
+});
+
+app.post("/addPackage", async (req, res) => {
+  const { name, monthlyFee, mbs } = req.body;
+  try {
+    const package = await Package.create({ name, monthlyFee, mbs });
+    res.send(package);
+  } catch (err) {
+    res.send("Error");
+    console.log(err.message);
+  }
+});
+
+app.get("/getAreas", async (req, res) => {
+  try {
+    let response = await Area.find().select("code name");
+
+    let areas = [];
+    for (i = 0; i < response.length; i++) {
+      obj = {
+        id: response[i]._id,
+        code: response[i].code,
+        name: response[i].name,
+        activeUsers: 2,
+        streets: 3,
+      };
+      areas.push(obj);
     }
 
-    })
+    // res.send(typeof(x))
+    res.send(areas);
+  } catch (error) {
+    res.send(error.message);
+  }
+});
+app.get("/getPackages", async (req, res) => {
+  try {
+    let response = await Package.find().select("name monthlyFee mbs");
 
-app.post('/addArea', async (req, res) => {
-        const {code, name} = req.body
-        try{
-            const area = await Area.create({code, name})
-            res.send(area)
-        }catch(err){
-            res.send("Error")
-            console.log(err.message)
-        }
-    
-        })
+    let packages = [];
+    for (i = 0; i < response.length; i++) {
+      obj = {
+        id: response[i]._id,
+        name: response[i].name,
+        monthlyFee: response[i].monthlyFee,
+        mbs: response[i].mbs,
+        activeUsers: 2,
+        streets: 3,
+      };
+      packages.push(obj);
+    }
 
-app.get('/getAreas', async (req, res) => {
-    try{
-        let response = await Area.find().select("code name")
-       
-        let areas = []
-        for (i=0;i<response.length;i++){
-            obj ={id: response[i]._id,
-            code:response[i].code,
-            name: response[i].name,
-            activeUsers: 2,
-            streets: 3,}
-            areas.push(obj)
-        }
-       
-
-       // res.send(typeof(x))
-        res.send(areas)
-
-
-    }catch(error){
-        res.send(error.message)
-    }    
-
-        
-})
-
-
+    // res.send(typeof(x))
+    res.send(packages);
+  } catch (error) {
+    res.send(error.message);
+  }
+});
 
 //DB Connection
-mongoose.connect('mongodb+srv://aneeqduraiz:unlock!!!@cluster0.ide38ku.mongodb.net/?retryWrites=true&w=majority')
-    .then(()=>{
-        console.log("Connected to Mongo Atlas")
-         app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-        // app.listen(3001, "192.168.100.57");
-    })
-    .catch((error)=>{
-        console.log("Fooking error in connection to mongo")
-
-    })
-
+mongoose
+  .connect(
+    "mongodb+srv://aneeqduraiz:unlock!!!@cluster0.ide38ku.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to Mongo Atlas");
+    app.listen(port, () =>
+      console.log(`Example app listening on port ${port}!`)
+    );
+    // app.listen(3001, "192.168.100.57");
+  })
+  .catch((error) => {
+    console.log("Fooking error in connection to mongo");
+  });
