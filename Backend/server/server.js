55d6069326898591fb198dedd252ac194ef36b39
AneeqDuraiz <aneeqduraiz456@gmail.com>
2022-12-31 08:43:21 +0500

    Implemented express.Router() for more readablilty

--- Backend/server/server.js
+++ Backend/server/server.js
@@ -1,84 +1,19 @@
 const express = require('express')
-const mongoose = require('mongoose')
-const Area = require('../Modles/areaModel')
 var cors = require('cors');
+const mongoose = require('mongoose')
+const areaRoutes = require("../routes/areaRoutes")
 
 
 const app = express()
 const port = 3001
-
-
 app.use(express.json())
 app.use(cors());
 
+
 //Routes
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.get('/', (req, res) => res.send('A Test String'))
-
-app.post('/testp', (req, res) => res.send('A Test String form post'))
-
-app.post('/addArea', async (req, res) => {
-    const {code, name} = req.body
-    try{
-        const area = await Area.create({code, name})
-        res.send(area)
-    }catch(err){
-        res.send("Error")
-        console.log(err.message)
-    }
-
-    })
-
-app.post('/addArea', async (req, res) => {
-        const {code, name} = req.body
-        try{
-            const area = await Area.create({code, name})
-            res.send(area)
-        }catch(err){
-            res.send("Error")
-            console.log(err.message)
-        }
-    
-        })
-
-app.get('/getAreas', async (req, res) => {
-    try{
-        let response = await Area.find().select("code name")
-       
-        let areas = []
-        for (i=0;i<response.length;i++){
-            obj ={id: response[i]._id,
-            code:response[i].code,
-            name: response[i].name,
-            activeUsers: 2,
-            streets: 3,}
-            areas.push(obj)
-        }
-       // res.send(typeof(x))
-        res.send(areas)
+app.use('/api/area', areaRoutes);    
 
 
-    }catch(error){
-        res.send(error.message)
-    }  
-        
-})
-
-app.delete('/deleteArea/:id', async(req, res) => {
-    const id = req.params.id
-
-    try{
-        const deletedArea = await Area.findOneAndDelete({_id: id})
-        if(!deletedArea){
-            res.send("No Area Found!")
-        }
-        res.send(deletedArea)
-
-    }catch(err){
-        res.send(error.message)
-    }
-})
 
 //DB Connection
 mongoose.connect('mongodb+srv://aneeqduraiz:unlock!!!@cluster0.ide38ku.mongodb.net/?retryWrites=true&w=majority')
