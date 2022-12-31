3c0c7371696f8d02a81389dc039615ae6208545a
Muhammad Shahnoor <SP20-BCS-137@CUILAHORE.EDU.PK>
2023-01-01 04:51:26 +0500

    Add useAreasContext hook for areas

---
+++ Frontend/src/app/main/hooks/useAreasContext.js
@@ -0,0 +1,12 @@
+import { AreasContext } from "../context/AreaContext";
+import { useContext } from "react";
+
+export const useAreasContext = () => {
+  const context = useContext(AreasContext);
+
+  if (!context) {
+    throw new Error(`useAreasContext must be used within a AreasContext`);
+  }
+
+  return context;
+};
