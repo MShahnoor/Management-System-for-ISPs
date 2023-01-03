7b6a4d6200210916c39ae869ff1286c8cc0c01d3
Muhammad Shahnoor <SP20-BCS-137@CUILAHORE.EDU.PK>
2023-01-04 00:20:04 +0500

    Add userRoutes in Backend/server

--- Backend/server/server.js
+++ Backend/server/server.js
@@ -1,33 +1,30 @@
-const express = require('express')
-var cors = require('cors');
-const mongoose = require('mongoose')
-const areaRoutes = require("../routes/areaRoutes")
+const express = require("express");
+var cors = require("cors");
+const mongoose = require("mongoose");
+const areaRoutes = require("../routes/areaRoutes");
+const userRoutes = require("../routes/userRoutes");
 
-
-const app = express()
-const port = 3001
-app.use(express.json())
+const app = express();
+const port = 3001;
+app.use(express.json());
 app.use(cors());
 
-
 //Routes
-app.use('/api/area', areaRoutes);    
-
-//patch Routes
-
-
-
+app.use("/api/area", areaRoutes);
+app.use("/api/user", userRoutes);
 
 //DB Connection
-mongoose.connect('mongodb+srv://aneeqduraiz:unlock!!!@cluster0.ide38ku.mongodb.net/?retryWrites=true&w=majority')
-    .then(()=>{
-        console.log("Connected to Mongo Atlas")
-         app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-        // app.listen(3001, "192.168.100.57");
-    })
-    .catch((error)=>{
-        console.log("Fooking error in connection to mongo")
-        console.log(error.message)
-
-    })
-
+mongoose
+  .connect(
+    "mongodb+srv://aneeqduraiz:unlock!!!@cluster0.ide38ku.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to Mongo Atlas");
+    app.listen(port, () =>
+      console.log(`Example app listening on port ${port}!`)
+    );
+  })
+  .catch((error) => {
+    console.log("Fooking error in connection to mongo");
+    console.log(error.message);
+  });
