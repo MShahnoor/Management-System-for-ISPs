2cce734d3489cd40fc0405a804c5261884715451
Hammad Nazir <hammadnazir106@gmail.com>
2023-01-05 22:20:41 -0800

    Add Route of Add, Delete and Update method in Package Route

--- Backend/routes/packageRoutes.js
+++ Backend/routes/packageRoutes.js
@@ -1,54 +1,20 @@
-const express = require("express");
 const { response } = require("express");
+const express = require("express");
+const mongoose = require("mongoose");
 const router = express.Router();
+const {
+  getPackages,
+  addPackage,
+  deletePackage,
+  editPackage,
+} = require("../Controllers/packageControllers");
 
-const Package = require("../Modles/packageModel");
+router.get("/getPackages", getPackages);
 
-router.get("/getPackages", async (req, res) => {
-  console.log("get req in packages");
-  try {
-    let response = await Package.find().select("name monthlyFee mbs");
+router.post("/addPackage", addPackage);
 
-    let packages = [];
-    for (i = 0; i < response.length; i++) {
-      obj = {
-        id: response[i]._id,
-        name: response[i].name,
-        monthlyFee: response[i].monthlyFee,
-        mbs: response[i].mbs,
-      };
-      packages.push(obj);
-    }
-    res.send(packages);
-  } catch (error) {
-    res.send(error.message);
-  }
-});
+router.delete("/deletePackage/:id", deletePackage);
 
-router.post("/addPackage", async (req, res) => {
-  console.log("Get Package");
-  const { name, monthlyFee, mbs } = req.body;
-  try {
-    const package = await Package.create({ name, monthlyFee, mbs });
-    res.send(package);
-  } catch (err) {
-    res.send("Error");
-    console.log(err.message);
-  }
-});
-
-router.delete("/deletePackage/:id", async (req, res) => {
-  const id = req.params.id;
-
-  try {
-    const deletedPackage = await Package.findOneAndDelete({ _id: id });
-    if (!deletedPackage) {
-      res.status(404).send("No Package Found!");
-    }
-    res.status(200).send(deletedPackage);
-  } catch (err) {
-    res.send(err.message);
-  }
-});
+router.patch("/editPackage/:id", editPackage);
 
 module.exports = router;
