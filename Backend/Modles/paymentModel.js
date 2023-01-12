b4dbf15d3300e7beb50d5e2857fc129589800dad
AneeqDuraiz <aneeqduraiz456@gmail.com>
2023-01-12 18:12:45 +0500

    Updated payment model for references

--- Backend/Modles/paymentModel.js
+++ Backend/Modles/paymentModel.js
@@ -1,6 +1,17 @@
 const mongoose = require('mongoose')
 
 paymentSchema = new mongoose.Schema({
+
+    areaCode: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Area'
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref:'User'
+    },
     serialNo: {
         type: Number,
         maxLength: 1,
